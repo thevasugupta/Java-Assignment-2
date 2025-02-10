d3342de5695b54ae185dc71d1428611b5fa23369
thevasugupta <thevasugupta04@gmail.com>
2025-02-10 23:13:49 +0530

    Q1

---
+++ utilities/Main.java
@@ -0,0 +1,9 @@
+import utilities.MathUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        MathUtils mathUtils = new MathUtils();
+        int sum = mathUtils.add(25, 17);
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ utilities/MathUtils.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class MathUtils {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
